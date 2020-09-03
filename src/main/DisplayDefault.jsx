73240dfc1fe219bf76f0b23c7ce53b8dd381bd61
Lauren Anthony <laurenanthony247@gmail.com>
2020-09-03 00:28:13 -0400

    deleted conditional className stuff

--- src/main/DisplayDefault.jsx
+++ src/main/DisplayDefault.jsx
@@ -8,31 +8,14 @@ import {
   CardTitle,
   CardText,
 } from "reactstrap";
-import { classNames } from "classnames";
 import EditNote from "./EditNote";
-
-const DisplayDefault = (props) => {
-  var arr = props.notes;
-  const computeClass = (val) => {
-    return classNames({
-      note1: val === 0 || val % 4 === 1,
-      note2: val === 1 || val % 4 === 2,
-      note3: val === 2 || val % 4 === 3,
-      note4: val === 3 || val % 4 === 0,
-    });
-  };
-
-  const noteClass = arr.forEach((index) => {
-    var val = index;
-    computeClass(val);
-  });
 
   const noteCardMapping = () => {
     return props.notes.map((note) => {
       return (
         <Col md="4" sm="6">
           <Card>
-            <CardBody className={noteClass}>
+            <CardBody>
               <CardTitle>{note.type}</CardTitle>
               <CardText>{note.details}</CardText>
               <EditNote
